1f8cbd41316c87e13ff60fb64bae33528e9386e9
Amey Thakur <ameythakur20@gmail.com>
2026-01-05 18:18:30 -0500

    CHESS

--- Source Code/app.js
+++ Source Code/app.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, './')
 
 app.use(express.static(publicDirectoryPath))
 
